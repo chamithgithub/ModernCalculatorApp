5d3fd4d68082a5b2f1b50bd9824ead3c21affe2a
chamithgithub <dchamith4@gmail.com>
2023-10-27 15:13:50 +0530

    update and add colors

---
+++ CalculatorApp/src/components/styles/Colors.tsx
@@ -0,0 +1,12 @@
+export const myColors = {
+  light: "#F1f2f3",
+  dark: "#17171c",
+  blue: "#4b5efc",
+  btnGray: "#4e505f",
+  btnDark: "#2e2f38",
+  color6: "#E74C3C",
+  color7: "#1ABC9C",
+  black: "#000000",
+  gray: "#34495E",
+  white: "#ffffff",
+};
